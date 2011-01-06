152954d223e2b2976f79005a6909234002b2ebe6
Caesar Kabalan <Dandelock@gmail.com>
2011-01-06 10:20:13 +0000

    Client & Lib: Majority of Registration page done. All user validation is solid. Redid the internal aspects of the interface. Most controls should use some sort of a style. Next Step: Sockets!
    git-svn-id: file:///content/svn/csharp/bandy/BandyLib@15 b4a9a19a-b4d8-4a93-b48d-c161a4a7b3ea

--- Functions.cs
+++ Functions.cs
@@ -4,6 +4,54 @@ using System.Linq;
 using System.Text;
 
 namespace BandyLib {
-	class Functions {
+	public static class Functions {
+
+		public static Boolean IsAlphaNumeric(String InputStr) {
+			if (string.IsNullOrEmpty(InputStr)) {
+				return false;
+			}
+			for (int i = 0; i < InputStr.Length; i++) {
+				if (!(Char.IsLetter(InputStr[i])) && (!(Char.IsNumber(InputStr[i])))) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+
+
+		public static Boolean HasNonKeyboardCharacters(String InputStr) {
+			int AsciiEquiv;
+			foreach (Char CurrentChar in InputStr.ToCharArray()) {
+				AsciiEquiv = (int)CurrentChar;
+				if ((AsciiEquiv < 32) || (AsciiEquiv > 126)) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public static Boolean ContainsAlpha(String InputStr) {
+			int AsciiEquiv;
+			foreach (Char CurrentChar in InputStr.ToCharArray()) {
+				AsciiEquiv = (int)CurrentChar;
+				if (((AsciiEquiv > 64) && (AsciiEquiv < 91)) || ((AsciiEquiv > 96) && (AsciiEquiv < 123))) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public static Boolean ContainsNumeric(String InputStr) {
+			int AsciiEquiv;
+			foreach (Char CurrentChar in InputStr.ToCharArray()) {
+				AsciiEquiv = (int)CurrentChar;
+				if ((AsciiEquiv > 47) && (AsciiEquiv < 58)) {
+					return true;
+				}
+			}
+			return false;
+		}
+
 	}
 }
